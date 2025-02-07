c12906c54766ef48d57e829523f86350314ce31e
natamatrix <nataseaside@gmail.com>
2025-02-07 11:53:26 +0200

    vers 1

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print (123)
+questions = {
+    "Сколько планет в Солнечной системе?": "8",
+    "Столица Франции?": "Париж"
+}
 
-print(321)
+score = 0
+for q, a in questions.items():
+    user_answer = input(q + " ")
+    if user_answer.lower() == a.lower():
+        print("Правильно!")
+        score += 1
+    else:
+        print("Неверно!")
+print(f"Ваш результат: {score} из {len(questions)}")
+
+
